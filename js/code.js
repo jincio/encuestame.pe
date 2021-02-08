17a1edbe07500faa6b3cfab5e9242ebab25fd64e
unknown <alejomayuri@gmail.com>
2021-02-08 13:32:33 -0500

    maqueta página de inicio

---
+++ js/code.js
@@ -0,0 +1,34 @@
+var st = $(window).scrollTop();
+    var b = 0;
+
+    $(window).scroll(function() {
+        if (st <= 500) {
+            if (b == 0) {
+                $('.counter').each(function() {
+                    var $this = $(this),
+                    countTo = $this.attr('data-count');
+
+                    $({ countNum: $this.text()}).animate({
+                        countNum: countTo
+                        },
+                        {
+                        duration: 1500,
+                        easing:'linear',
+                        step: function() {
+                        $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function() {
+                        $this.text(this.countNum);
+                        //alert('finished');
+                        }
+                    });  
+                });
+                b = 1;
+            }else {
+
+            }
+        }else {
+
+        }
+
+    });
